c6e27f3a3e24042ff4972584795cb9cbf9beb81a
namlev <mikipatkowski@protonmail.com>
2025-07-04 18:12:20 +0200

    Add kotest and DB dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,34 @@ repositories {
 }
 
 dependencies {
+    // Spring
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    // Database
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("com.h2database:h2:2.3.232")
+
+    // Arrow
+    implementation("io.arrow-kt:arrow-core:2.0.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.mockk:mockk:1.13.17")
+
+    // Kotest
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.0")
+    testImplementation("io.kotest:kotest-property:5.9.0")
+    implementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+    testImplementation("io.kotest.extensions:kotest-assertions-arrow:2.0.0")
+
+    // Security, JWT
+//    implementation("org.springframework.boot:spring-boot-starter-security")
+//    implementation("com.auth0:java-jwt:4.5.0")
+//    testImplementation("org.springframework.security:spring-security-test:6.4.5")
 }
 
 kotlin {
