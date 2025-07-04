47f0ce86a19b3face4b2b2ea7f5ad2630d91fbf1
namlev <mikipatkowski@protonmail.com>
2025-07-04 18:05:13 +0200

    Repo init

---
+++ src/main/kotlin/com/coursy/courses/CoursesApplication.kt
@@ -0,0 +1,11 @@
+package com.coursy.courses
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoursesApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoursesApplication>(*args)
+}
